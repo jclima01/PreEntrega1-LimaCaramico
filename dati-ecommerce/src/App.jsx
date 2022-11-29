812b42cb27fb3ba5c023ed5dcc413741c5d9744d
JuanCruz <juanlimacaramico@gmail.com>
2022-11-29 17:56:06 -0300

    first commit

---
+++ dati-ecommerce/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+// import './App.css'
+import NavBar from "./components/navbar/NavBar"
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+
+
+  return (
+    <div>
+      <NavBar />
+      
+    </div>
+  )
+}
+
+export default App
---
+++ dati-ecommerce/src/components/navbar/NavBar.jsx
@@ -0,0 +1,49 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import CartWidget from "../cartWidget/CartWidget";
+
+function NavBar() {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <a href="https://ibb.co/cCN8g7S">
+          <img
+            src="https://i.ibb.co/cCN8g7S/logoDati.jpg"
+            alt="logoDati"
+            border="0"
+            width="50"
+            height="50"
+          />
+        </a>
+        <Navbar.Brand href="#home">Dati Pizzería</Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Quienes somos</Nav.Link>
+            <Nav.Link href="#pricing">Hacé tu pedido</Nav.Link>
+            <NavDropdown
+              title="Nuestros productos"
+              id="collasible-nav-dropdown"
+            >
+              <NavDropdown.Item href="#action/3.1">Pizzas</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Calzones</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Empanadas</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">Bebidas</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">
+              <CartWidget />
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
